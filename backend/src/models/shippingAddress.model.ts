37d05015a00f4ff27547a54b361d6273c77d8392
AcePeQ <maciej.nojszewski22@gmail.com>
2025-03-21 13:24:35 +0100

    feat: fix required issue with moongose schema

--- backend/src/models/shippingAddress.model.ts
+++ backend/src/models/shippingAddress.model.ts
@@ -12,13 +12,13 @@ export interface IShippingAddress extends Document {
 
 const shippingAddressSchema = new Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
-  city: { type: String, required: true },
-  zipCode: { type: String, required: true },
-  address: { type: String, required: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  email: { type: String },
+  phoneNumber: { type: String },
+  city: { type: String },
+  zipCode: { type: String },
+  address: { type: String },
 });
 
 const ShippmentAddress = mongoose.model(
