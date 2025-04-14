bde0c1470f16d775e9deb429cbe11ecf02ccce17
AcePeQ <maciej.nojszewski22@gmail.com>
2025-04-14 18:58:17 +0200

    feat: pizza card

--- frontend/src/features/Menu/Menu.tsx
+++ frontend/src/features/Menu/Menu.tsx
@@ -1,7 +1,7 @@
 function Menu() {
   return (
     <section className="my-10">
-      <h2 className="text-center font-header text-4xl/tight font-bold text-pretty mb-18">
+      <h2 className="text-center font-header text-4xl/tight font-bold text-pretty mb-42">
         Pizza Menu
       </h2>
 
@@ -18,6 +18,31 @@ function Menu() {
                 Margharita
               </figcaption>
             </figure>
+          </div>
+          <div className="bg-stone-800 rounded-md w-1 h-full absolute -top-1/2 left-1/2 -translate-x-1/2">
+            <div className="absolute ml-2 bg-stone-800 rounded-xl px-3 py-1 top-0 left-0 after:content-[''] after:rounded-[50%] after:absolute after:-left-[11px] after:top-1/2 after:-translate-y-1/2 after:w-2.5 after:h-2.5 after:bg-green-800 text-amber-50 text-xl tracking-wide">
+              Pomidor
+            </div>
+
+            <div className="absolute ml-2 bg-stone-800 rounded-xl px-3 py-1 top-12 left-0 after:content-[''] after:rounded-[50%] after:absolute after:-left-[11px] after:top-1/2 after:-translate-y-1/2 after:w-2.5 after:h-2.5 after:bg-green-800 text-amber-50 text-xl tracking-wide">
+              Pomidor
+            </div>
+
+            <div className="absolute ml-2 bg-stone-800 rounded-xl px-3 py-1 top-24 left-0 after:content-[''] after:rounded-[50%] after:absolute after:-left-[11px] after:top-1/2 after:-translate-y-1/2 after:w-2.5 after:h-2.5 after:bg-green-800 text-amber-50 text-xl tracking-wide">
+              Pomidor
+            </div>
+
+            <div className="absolute ml-2 bg-stone-800 rounded-xl px-3 py-1 top-36 left-0 after:content-[''] after:rounded-[50%] after:absolute after:-left-[11px] after:top-1/2 after:-translate-y-1/2 after:w-2.5 after:h-2.5 after:bg-green-800 text-amber-50 text-xl tracking-wide">
+              Pomidor
+            </div>
+
+            <div className="absolute ml-2 bg-stone-800 rounded-xl px-3 py-1 top-48 left-0 after:content-[''] after:rounded-[50%] after:absolute after:-left-[11px] after:top-1/2 after:-translate-y-1/2 after:w-2.5 after:h-2.5 after:bg-green-800 text-amber-50 text-xl tracking-wide">
+              Pomidor
+            </div>
+
+            <div className="absolute ml-2 bg-stone-800 rounded-xl px-3 py-1 top-60 left-0 after:content-[''] after:rounded-[50%] after:absolute after:-left-[11px] after:top-1/2 after:-translate-y-1/2 after:w-2.5 after:h-2.5 after:bg-green-800 text-amber-50 text-xl tracking-wide">
+              Pomidor
+            </div>
           </div>
         </li>
       </ul>
