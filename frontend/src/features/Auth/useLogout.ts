c11613665baf973457c1ec8b8625eae7f4529f51
AcePeQ <maciej.nojszewski22@gmail.com>
2025-05-08 15:56:31 +0200

    navigate to logout

--- frontend/src/features/Auth/useLogout.ts
+++ frontend/src/features/Auth/useLogout.ts
@@ -2,13 +2,16 @@ import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useUserStore } from "../../store/useUserStore";
 import { logoutFromAccountApi } from "../../services/apiAccount";
+import { useNavigate } from "react-router";
 
 export function useLogout() {
+  const navigate = useNavigate();
   const { logout } = useUserStore();
   const { isPending: isLoggingOut, mutate: logoutFromAccount } = useMutation({
     mutationFn: () => logoutFromAccountApi(),
     onSuccess: () => {
       toast.success("Successfully logged out");
+      navigate("/", { replace: true });
       logout();
     },
   });
