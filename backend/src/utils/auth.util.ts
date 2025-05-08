b387f91548691bfc5e105dc3244898bbcdf03506
AcePeQ <maciej.nojszewski22@gmail.com>
2025-05-08 15:04:55 +0200

    token fix

--- backend/src/utils/auth.util.ts
+++ backend/src/utils/auth.util.ts
@@ -2,6 +2,8 @@ import { Response } from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
+// sameSite = dev = strict
+
 export const generateToken = (
   userId: mongoose.Types.ObjectId | undefined,
   res: Response
@@ -13,7 +15,7 @@ export const generateToken = (
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "none",
     secure: process.env.NODE_ENV !== "development",
   });
 
