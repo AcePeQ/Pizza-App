f64d38c20e6c4baced109c3b200d2e098282f938
AcePeQ <maciej.nojszewski22@gmail.com>
2025-05-08 14:26:36 +0200

    build

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ app.use(express_1.default.json({ limit: "10mb" }));
 app.use(express_1.default.urlencoded({ extended: true, limit: "10mb" }));
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
 }));
 app.use("/api/auth", auth_route_1.default);
