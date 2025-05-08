5ff22cc98d005def626da592b00bdb959ec3e934
AcePeQ <maciej.nojszewski22@gmail.com>
2025-05-08 15:05:41 +0200

    token fix

--- backend/src/utils/auth.util.js
+++ backend/src/utils/auth.util.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateToken = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+// sameSite = dev = strict
 const generateToken = (userId, res) => {
     const token = jsonwebtoken_1.default.sign({ userId }, process.env.JWT_SECRET_KEY, {
         expiresIn: "7d",
@@ -12,7 +13,7 @@ const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
         secure: process.env.NODE_ENV !== "development",
     });
     return token;
